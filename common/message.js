dd70f3bdcde97c69dede373f7d7c2af021620fa2
lmh2072005 <lmh2072005@126.com>
2015-01-23 18:38:30 +0800

    update msg box

--- common/message.js
+++ common/message.js
@@ -1,26 +1,27 @@
 function showMsgFn(opts) {
     var defaultOpts = {
-        area: $(document),
-        msg: '',
-        type: 'warning',
-        css: {},
-        duration: 1000,
-        callback: null,
-        hideType: 'fadeOut'
-    },
-    hideType = {
-        hide: function (obj) { obj.hide(); },
-        fadeOut: function (obj) { obj.fadeOut(); },
-        fadeUp: function (obj) {
-            obj.animate({ 'top': '-=40px', 'opacity': 0}, 2000, function () {
-                obj.hide();
-            });
-        }
-    },
-    msgTypeColor = {
-        warning : '#ff3300',
-        success: '#20a936'
-    };
+            area: $(document),
+            msg: '',
+            type: 'warning',
+            position: 'after',//over、after
+            css: {},
+            duration: 1000,
+            callback: null,
+            hideType: 'fadeOut'
+        },
+        hideType = {
+            hide: function (obj) { obj.hide(); },
+            fadeOut: function (obj) { obj.fadeOut(); },
+            fadeUp: function (obj) {
+                obj.animate({ 'top': '-=40px', 'opacity': 0 }, 2000, function () {
+                    obj.hide();
+                });
+            }
+        },
+        msgTypeColor = {
+            warning: '#ff3300',
+            success: '#20a936'
+        };
     opts = $.extend(defaultOpts, opts);
     var area = $(opts.area);
     var submitMsgBox = $('#uiShowMsgBox'), areaOffset = area.offset();
@@ -30,12 +31,16 @@ function showMsgFn(opts) {
     } else {
         submitMsgBox.html(opts.msg);
     }
+    var msgLeft = areaOffset.left;
+    if (opts.position === 'after') {
+        msgLeft += area.outerWidth();
+    }
     submitMsgBox.css({
         'position':'absolute',
         'text-align':'center',
         'font-size':'16px',
         'opacity':1,
-        'left': areaOffset.left,
+        'left': msgLeft,
         'top': areaOffset.top,
         'width': area.outerWidth(),
         'height': area.outerHeight(),
